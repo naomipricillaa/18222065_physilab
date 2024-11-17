6540265e7d07b6bb4aa29ef2f029f775b4e3f524
naomipricillaa <naaooommiii16@gmail.com>
2024-11-17 17:44:39 +0700

    static lagi

--- physilab/physilab/settings.py
+++ physilab/physilab/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5e*go*0y31w=rrdu*(j9a3wk!6+t5k9e98ad@-fh&*vu8+#)ib'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'localhost', '127.0.0.1']
 
@@ -131,11 +131,11 @@ USE_TZ = True
 LOGIN_URL = 'login'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'static'
+static_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
-STATIC_STORAGE = 'whitenoise.storage.CompressedManifeststaticStorage'
+static_STORAGE = 'whitenoise.storage.CompressedManifeststaticStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
